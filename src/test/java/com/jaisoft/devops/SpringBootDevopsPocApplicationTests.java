63244193dfcd86b833c32bf07c34cf0b2bdb66c7
Jaime Gómez Moraleda <jaime.gomez.moraleda@gmail.com>
2020-09-05 14:16:23 +0200

    first commit

---
+++ src/test/java/com/jaisoft/devops/SpringBootDevopsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaisoft.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevopsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
